34bf912a5d48a00d7d5de1d1e09d304e6832c9da
FahimKamal <fahimkamal63@gmail.com>
2020-01-18 21:23:38 +0600

    Space Invader v1.0 Under development The game has enemy and can move left and right. both of them has border detection.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Space Invader
 Created by: Fahim kamal
 Date: 17.01.2020
 """
+import random
+
 import pygame
 
 # Initialize the pygame
@@ -24,9 +26,20 @@ player_x = 350
 player_y = 500
 player_x_change = 0
 
+
+# Set the Enemy
+enemy_img = pygame.image.load('enemy.png')
+enemy_x = random.randint(0, 736)
+enemy_y = random.randint(50, 150)
+enemy_x_change = 0.3
+
 def player():
     """place the player at a specific location"""
     screen.blit(player_img, (player_x, player_y))
+
+def enemy():
+    """place the player at a specific location"""
+    screen.blit(enemy_img, (enemy_x, enemy_y))
 
 
 game_running = True
@@ -49,18 +62,40 @@ while game_running:
             # if left arrow is pressed
             if event.key == pygame.K_LEFT:
                 print('Left key is pressed')
-                player_x_change += -0.5
+                player_x_change = -0.5
 
             # if right arrow is pressed
             if event.key == pygame.K_RIGHT:
                 print('Right key is pressed')
-                player_x_change += 0.5
+                player_x_change = 0.5
 
         # Check if any key is released after pressing
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 print('keystroke is released')
                 player_x_change = 0
+
+    # Change the player location
     player_x += player_x_change
+
+    # border check for player
+    if player_x <= 0:
+        # Left border
+        player_x = 0
+    elif player_x >= 736:
+        # Right border
+        player_x = 736
+
+    # Border check for enemy
+    if enemy_x <= 0:
+        enemy_x_change *= -1
+        enemy_y += 36
+    elif enemy_x > 736:
+        enemy_x_change *= -1
+        enemy_y += 36
+
+    enemy_x += enemy_x_change
+
     player()
+    enemy()
     pygame.display.update()
