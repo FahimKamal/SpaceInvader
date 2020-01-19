7d705de536575115b31b00ecd07d969430d08080
FahimKamal <fahimkamal63@gmail.com>
2020-01-19 17:48:19 +0600

    Space Invader v1.0 Under development The game has a background image. player can fire bullet, working on bullet and enemy collision.

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 # Set screen color RGB - Red, Green, Blue
 screen.fill((0, 0, 0))
+# Background image
+background = pygame.image.load('gamebg.gif')
 
 # Set the player
 player_img = pygame.image.load('spaceship.png')
@@ -26,20 +28,31 @@ player_x = 350
 player_y = 500
 player_x_change = 0
 
-
 # Set the Enemy
 enemy_img = pygame.image.load('enemy.png')
 enemy_x = random.randint(0, 736)
 enemy_y = random.randint(50, 150)
 enemy_x_change = 0.3
 
+# Set the bullet
+bullet_img = pygame.image.load('bullet.png')
+bullet_x = 0
+bullet_y = 500
+bullet_y_change = 0.7
+bullet_state = 'ready'
+
+
 def player():
     """place the player at a specific location"""
     screen.blit(player_img, (player_x, player_y))
 
 def enemy():
-    """place the player at a specific location"""
+    """place the enemy at a specific location"""
     screen.blit(enemy_img, (enemy_x, enemy_y))
+
+def fire():
+    global bullet_state
+    screen.blit(bullet_img, (bullet_x + 16, bullet_y + 10))
 
 
 game_running = True
@@ -48,6 +61,8 @@ game_running = True
 while game_running:
     # Set screen color RGB - Red, Green, Blue
     screen.fill((0, 0, 0))
+    # set background
+    screen.blit(background, (0, 0))
     # Iterate through all events in pygame
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -69,6 +84,13 @@ while game_running:
                 print('Right key is pressed')
                 player_x_change = 0.5
 
+            # press space to fire bullet
+            if event.key == pygame.K_SPACE:
+                if bullet_state == 'ready':
+                    bullet_y = player_y
+                    bullet_x = player_x
+                    bullet_state = 'fire'
+
         # Check if any key is released after pressing
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -86,6 +108,8 @@ while game_running:
         # Right border
         player_x = 736
 
+    player()
+
     # Border check for enemy
     if enemy_x <= 0:
         enemy_x_change *= -1
@@ -95,7 +119,13 @@ while game_running:
         enemy_y += 36
 
     enemy_x += enemy_x_change
-
-    player()
     enemy()
+
+    if bullet_state == 'fire':
+        bullet_y -= bullet_y_change
+        fire()
+    
+    # border check for bullet
+    if bullet_y < 0:
+        bullet_state = 'ready'
     pygame.display.update()
